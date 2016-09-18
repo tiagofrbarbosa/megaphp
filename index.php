a350c639a47027045d24bdb187320a08b5d7270b
tiago Barbosa <tiagofr.barbosa@hotmail.com.br>
2016-09-18 10:15:33 -0300

    Atualizando

--- index.php
+++ index.php
@@ -1,41 +1,59 @@
-
 <?php
-
 define("singularity", 10);
 define("chaos_theory", 5);
 define("big_bang", 1);
 define("big_crunch", 60);
-
 $black_hole = 0;
-
+$revolver[] = 0;
 while($black_hole<singularity){
-
 			for($i=0;$i<=chaos_theory;$i++){
-
 			dejavu:
 			$bullet = rand(big_bang,big_crunch);
 			if(in_array($bullet, $revolver)){
 					goto dejavu;
 			}
 		 
-
 $revolver[$i] = $bullet;
-
 if($i==chaos_theory){
 $game = $revolver;
 sort($game);
-
 for($i=0;$i<=chaos_theory;$i++){
 print($game[$i] . " ");
 if($i==chaos_theory){
 ?><br><?php
+       $crawler = chaos_theory - chaos_theory;
+       $gsearch = array_slice($game,$crawler,chaos_theory+1);
+       $vacuum = implode($gsearch);
+          //for($b=$black_hole;$b=0;$b--){}
 		 }
-
 		}
 	}		
 }
 $black_hole++;
 }
+
+print('************************');
+?><br><?php
+print($crawler . ' ' .chaos_theory);
+?><br><?php
+print_r($gsearch);
+?><br><?php
+print($vacuum);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
